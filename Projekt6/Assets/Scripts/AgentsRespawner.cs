e5a5fc3dc127d2d9a8773d3e62df3c316f96155b
piotrwojtaszek <piowoj98@gmail.com>
2020-03-13 12:06:28 +0100

    Base

---
+++ Projekt6/Assets/Scripts/AgentsRespawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentsRespawner : MonoBehaviour
+{
+    public GameObject agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int count = 0;
+        for(int j = 3; j > -7; j--)
+        {
+            
+            for (int i = -16; i < 62; i++)
+            {
+                count++;
+                Vector3 rand = new Vector3(i, 0, j);
+                GameObject temp = Instantiate(agent, transform.position + rand, transform.rotation);
+            }
+
+        }
+        Debug.Log(count);
+    }
+    
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Projekt6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    private NavMeshAgent agent;
+    public void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(new Vector3(transform.position.x+ Random.Range(-2f, 2f), transform.position.y, Random.Range(34f,42f)));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }*/
+    }
+}
